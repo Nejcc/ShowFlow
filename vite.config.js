3ae5515d7c2b29f43ec9f7817b57444209850f6d
neo <info@after.si>
2025-04-18 22:06:59 +0200

    chore: update project configuration and dependencies

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,12 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.scss', 'resources/js/app.js'],
             refresh: true,
         }),
         vue(),
     ],
+    css: {
+        postcss: './postcss.config.js',
+    },
 });
